55a1a4f70c8d89a31133d0f2fc73271adca29491
Stryzhonok Danila <danstrig2000@mail.ru>
2020-01-12 03:45:50 +0300

    Yay!
    Upgraded versions of Stack and Queue, but they are not still perfect and will be updated soon as I hope

---
+++ Queue/index.js
@@ -0,0 +1,42 @@
+var Queue = function() {
+    this.storage = {};
+    this.head = 0;
+    this.tail = 0;
+}
+
+// adding element
+Queue.prototype.enqueue = function(value) {
+    this.storage[this.tail++] = value;
+    return this.size();
+}
+
+// element leaving the queue and returns value of the element
+Queue.prototype.dequeue = function() {
+    if (this.count == 0) 
+        return 'it is not possible to delete element from empty Queue';
+    var value = this.storage[this.head];
+    delete this.storage[this.head++];
+    return value;
+}
+
+// size of queue
+Queue.prototype.size = function() {
+    return this.tail - this.head;
+}
+
+// returns value of element which is first candidate to leave from queue
+Queue.prototype.peek = function() {
+    return this.storage[this.head];
+}
+
+// returns bool which answers the question does contain queue the value or not
+Queue.prototype.contains = function (value) {
+    return Object.values(this.storage).includes(value);
+}
+
+// returns a number of first element with this value in queue
+Queue.prototype.positionInQueue = function(value) {
+    return Object.values(this.storage).indexOf(value) + 1;
+}
+
+module.exports = Queue;
---
+++ Stack/index.js
@@ -0,0 +1,32 @@
+// peek() similar as a pop but does not remove an item
+// pop() removes last added item
+//push(value) adds value and returns size
+//size() returns size
+
+var Stack = function() {
+    this.storage = {};
+    this.count = 0;
+}
+
+Stack.prototype.push = function(value) {
+    this.storage[this.count++] = value;
+    return this.size();
+}
+
+Stack.prototype.size = function() {
+    return this.count;
+}
+
+Stack.prototype.peek = function() {
+    return this.storage[this.count - 1];
+}
+
+Stack.prototype.pop = function() {
+    if (this.size() == 0) 
+        return 'It is not possible to delete element when Stack is empty';
+    var value = this.storage[this.count - 1]
+    delete this.storage[--this.count];
+    return value;
+}
+
+module.exports = Stack;
